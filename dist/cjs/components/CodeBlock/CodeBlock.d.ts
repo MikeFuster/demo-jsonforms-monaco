a018435e8a028a2820dc54c93176503d5973cf66
MikeFuster <mfuster@litebox.ai>
2022-08-01 13:20:55 -0300

    add gitignore

--- dist/cjs/components/CodeBlock/CodeBlock.d.ts
+++
@@ -1,8 +0,0 @@
-interface Props {
-    defaultValue: string;
-    onChange: (val: string) => void;
-    label?: string;
-    isExpandable?: boolean;
-}
-declare const CodeBlockEditor: ({ defaultValue, onChange, label, isExpandable, }: Props) => JSX.Element;
-export default CodeBlockEditor;
